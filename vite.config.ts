662827eaa6c8956fdb501a3093df99ac42780d90
Aanand Patel <aanandp2003ca@gmail.com>
2025-04-08 15:16:22 -0500

    installed shadcn+tailwind. temp component added to app.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 /// <reference types="vitest" />s
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: { globals: true, environment: 'jsdom' },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
