34308b75e75af2c5d80fc4043212dd58188c2014
laurasfelix <laurasfelix@hotmail.com.br>
2025-04-09 00:03:21 -0500

    adding rewards page, making page more responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import './App.css';
 import './index.css';
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar';
+import Layout from './components/Layout/Layout';
+import FoundBugs from './pages/FoundBugs/FoundBugs';
+import Rewards from './pages/Rewards/Rewards';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Rewards />} />
+            <Route path="bugs" element={<FoundBugs />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
